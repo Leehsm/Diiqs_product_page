189d7c7ae1537c005aa5249a9f6d9a115c0068d5
Leehsm <shahzleehasimwork@gmail.com>
2022-11-03 17:05:40 +0800

    admin header, footer, sidebar

---
+++ resources/views/backend/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top footer">
+        <div class="col-md-4 d-flex align-items-center">
+            <span class="mb-3 mb-md-0 text-muted">© 2022 DIIQS Beauty Enterprise</span>
+        </div>
+    </footer>
+</div>
---
+++ resources/views/backend/layout/sidebar.blade.php
@@ -0,0 +1,66 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="#">
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Orders
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Customers
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Sale Reports
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Feedback
+                </a>
+            <li class="nav-item">
+                <a class="nav-link" >
+                    {{-- Integrations --}}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" >
+                    {{-- Integrations --}}
+                </a>
+            </li>
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Frontend Infomation</span>
+        </h6>
+        <ul class="nav flex-column mb-2">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('manage-product-and-combo') }}">
+                    Products & Combo
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('manage-slider') }}">
+                    Sliders
+                </a>
+                </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">
+                About us
+            </a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">
+                About Product
+            </a>
+            </li>
+        </ul>
+    </div>
+</nav>
