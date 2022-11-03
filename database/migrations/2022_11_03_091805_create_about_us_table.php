10f423e628c1324d7a9fdf28842bd0b8620beb49
Leehsm <shahzleehasimwork@gmail.com>
2022-11-03 17:43:58 +0800

    about us migration file

---
+++ database/migrations/2022_11_03_091805_create_about_us_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutUsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('about_us', function (Blueprint $table) {
+            $table->id();
+            $table->string('description_line_1');
+            $table->string('description_line_2');
+            $table->string('description_line_3');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('about_us');
+    }
+}
