10ca965b423ceaa63e10f1eaebce5c09c27875e1
Leehsm <shahzleehasimwork@gmail.com>
2022-11-04 16:32:44 +0800

    create route for crud function admin page
    product and combo, about product, about us and slider

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\backend\AdminProductController;
 use App\Http\Controllers\backend\AdminSliderController;
 use App\Http\Controllers\backend\AdminAboutUsController;
 use App\Http\Controllers\backend\AdminAboutProductController;
+use App\Http\Controllers\backend\AdminFeedbackController;
 
 /*
 |--------------------------------------------------------------------------
@@ -38,21 +39,44 @@ Route::get('/admin/logout',[AdminController::class, 'destroy'])->name('admin.log
 Route::prefix('product_and_combo')->group(function(){
     Route::get('/view',[AdminProductController::class, 'view'])->name('manage-product-and-combo');
     Route::get('/create',[AdminProductController::class, 'create'])->name('create-product-and-combo');
+    Route::post('/store',[AdminProductController::class, 'store'])->name('store-product-and-combo');
+    Route::get('/edit/{id}', [AdminProductController::class, 'edit'])->name('edit-product-and-combo');
+    Route::post('/update', [AdminProductController::class, 'update'])->name('update-product-and-combo');
+    Route::get('/delete/{id}', [AdminProductController::class, 'delete'])->name('delete-product-and-combo'); 
 });
 
 Route::prefix('slider')->group(function(){
     Route::get('/view',[AdminSliderController::class, 'view'])->name('manage-slider');
     Route::get('/create',[AdminSliderController::class, 'create'])->name('create-slider');
+    Route::post('/store',[AdminSliderController::class, 'store'])->name('store-slider');
+    Route::get('/edit/{id}', [AdminSliderController::class, 'edit'])->name('edit-slider');
+    Route::post('/update', [AdminSliderController::class, 'update'])->name('update-slider');
+    Route::get('/delete/{id}', [AdminSliderController::class, 'delete'])->name('delete-slider'); 
 });
 
 Route::prefix('about_us')->group(function(){
     Route::get('/view',[AdminAboutUsController::class, 'view'])->name('manage-about-us');
     Route::get('/create',[AdminAboutUsController::class, 'create'])->name('create-about-us');
+    Route::post('/store',[AdminAboutUsController::class, 'store'])->name('store-about-us');
+    Route::get('/edit/{id}', [AdminAboutUsController::class, 'edit'])->name('edit-about-us');
+    Route::post('/update', [AdminAboutUsController::class, 'update'])->name('update-about-us');
+    Route::get('/delete/{id}', [AdminAboutUsController::class, 'delete'])->name('delete-about-us'); 
 });
 
 Route::prefix('about_Product')->group(function(){
     Route::get('/view',[AdminAboutProductController::class, 'view'])->name('manage-about-product');
     Route::get('/create',[AdminAboutProductController::class, 'create'])->name('create-about-product');
+    Route::post('/store',[AdminAboutProductController::class, 'store'])->name('store-about-product');
+    Route::get('/edit/{id}', [AdminAboutProductController::class, 'edit'])->name('edit-about-product');
+    Route::post('/update', [AdminAboutProductController::class, 'update'])->name('update-about-product');
+    Route::get('/delete/{id}', [AdminAboutProductController::class, 'delete'])->name('delete-about-product'); 
+});
+
+Route::prefix('feedback')->group(function(){
+    Route::get('/view',[AdminFeedbackController::class, 'view'])->name('manage-feedback');
+    Route::get('/edit/{id}', [AdminFeedbackController::class, 'edit'])->name('edit-feedback');
+    Route::post('/update', [AdminFeedbackController::class, 'update'])->name('update-feedback');
+    Route::get('/delete/{id}', [AdminFeedbackController::class, 'delete'])->name('delete-feedback'); 
 });
 
 
